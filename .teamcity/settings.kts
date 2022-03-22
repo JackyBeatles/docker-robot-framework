08c976ce81711a4dbd48baf61402e419b772a34f
kamchai.boo <kamchai.boo@lightnet.io>
2022-03-22 20:44:56 +0700

    fix test 145

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -41,7 +41,7 @@ object Build : BuildType({
     steps {
         script {
             name = "Robot Test"
-            scriptContent = "docker run --rm --shm-size=1g -e BROWSER=firefox -v %system.teamcity.build.checkoutDir%/test:/opt/robotframework/tests -v %system.teamcity.build.checkoutDir%/report:/opt/robotframework/reports ppodgorsek/robot-framework:latest"
+            scriptContent = "docker run --rm --shm-size=1g -e BROWSER=firefox -v %system.teamcity.build.checkoutDir%/test:/opt/robotframework/tests ppodgorsek/robot-framework:latest"
             
         }
     }
