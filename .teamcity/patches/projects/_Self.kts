ea4b687ad52dd8b6eda82ae4d3f04e1f1bed5366
JackyBeatles <JackyBeatles@TeamCity>
2022-03-22 14:25:27 +0000

    TeamCity change in 'Docker Robot Framework' project: Project report tab with title "report" was updated

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -19,7 +19,7 @@ changeProject(DslContext.projectId) {
                 title = "report"
                 startPage = "report.html"
                 buildType = "DockerRobotFramework_Build"
-                sourceBuildRule = ProjectReportTab.SourceBuildRule.LAST_SUCCESSFUL
+                sourceBuildRule = ProjectReportTab.SourceBuildRule.LAST_PINNED
                 sourceBuildBranchFilter = "+:<default>"
             }
         }
